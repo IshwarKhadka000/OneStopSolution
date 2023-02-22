5bac7ca51a6551df805ed3174f9a8ab7257388c1
ishwarkhadka000 <ishwarkhadka000@gmail.com>
2023-02-22 20:34:28 +0545

    view and update profile completed

--- userapp/views.py
+++ userapp/views.py
@@ -18,19 +18,97 @@ from userapp.forms import *
 class IndexView(TemplateView):
     template_name = 'pages/index.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['jobs'] = Job.objects.all()
+        return context
+
 
 class AboutView(TemplateView):
     template_name = 'pages/about.html'
 
 
-class ProfileView(TemplateView):
-    template_name = 'pages/profile.html'
+class SettingView(DetailView):
+    model = User
+    template_name = 'pages/settings.html'
+    context_object_name = 'user'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['services'] = Service.objects.all()
+        context['skills'] = Skill.objects.all()
+        return context
+
 
 
 class JobListView(ListView):
     model = Job
     template_name = 'pages/job-list1.html'
     context_object_name = 'jobs'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['services'] = Service.objects.all()
+        context['skills'] = Skill.objects.all()
+        return context
+
+
+class FilterJobListView(View):
+    template_name = 'pages/filtered_joblist.html'
+
+    def get_experience_queryset(self, value):
+        queryset = Profile.objects.all()
+        if value == 1:
+            queryset = set(queryset.filter(experience__lte=1).values_list('id', flat=True))
+        if value == 2:
+            queryset = set(queryset.filter(experience__lte=3, experience__gte=1).values_list('id', flat=True))
+        if value == 3:
+            queryset = set(queryset.filter(experience__lte=5, experience__gte=3).values_list('id', flat=True))
+        if value == 4:
+            queryset = set(queryset.filter(experience__gte=5).values_list('id', flat=True))
+        print(queryset)
+        return queryset
+
+    def get_queryset(self):
+        queryset = Job.objects.all()
+        if self.request.GET:
+            minimum_fee = self.request.GET.get('minimum_fee')
+            maximum_fee = self.request.GET.get('maximum_fee')
+            service = list(map(int, self.request.GET.getlist('service[]')))
+            experience = list(map(int, self.request.GET.getlist('experience[]')))
+            skill = list(map(int, self.request.GET.getlist('skill[]')))
+            if experience:
+                main_set = set()
+                set_1 = set()
+                set_2 = set()
+                set_3 = set()
+                set_4 = set()
+                if 1 in experience:
+                    set_1 = self.get_experience_queryset(1)
+                if 2 in experience:
+                    set_2 = self.get_experience_queryset(2)
+                if 3 in experience:
+                    set_3 = self.get_experience_queryset(3)
+                if 4 in experience:
+                    set_4 = self.get_experience_queryset(4)
+                main_set = main_set.union(set_1, set_2, set_3, set_4)
+
+                queryset = queryset.filter(id__in=main_set)
+
+            queryset = queryset.filter(payment__gte=minimum_fee, payment__lte=maximum_fee).distinct()
+            if service:
+                queryset = queryset.filter(service__in=service).distinct()
+
+            if skill:
+                queryset = queryset.filter(skill__in=skill).distinct()
+        return queryset
+
+    def get(self, request, *args, **kwargs):
+        context = dict()
+        context['jobs'] = self.get_queryset()
+        context['services'] = Service.objects.all()
+        context['skills'] = Skill.objects.all()
+        return render(request, self.template_name, context)
 
 
 class TalentListView(ListView):
@@ -60,6 +138,7 @@ class FilterTalentListView(View):
             queryset = set(queryset.filter(experience__gte=5).values_list('id', flat=True))
         print(queryset)
         return queryset
+
     def get_queryset(self):
         queryset = Profile.objects.all()
         if self.request.GET:
@@ -188,6 +267,40 @@ class BecomeWorkerView(FormView):
         messages.success(self.request, 'Registration details submitted successfully')
         return redirect('index')
 
+
+
+class WorkerProfileUpdateView(UpdateView):
+    form_class = WorkerProfileForm
+    queryset = Profile.objects.all()
+    template_name = 'pages/settings.html'
+    success_url = reverse_lazy('settings')
+
+    def form_valid(self, form):
+        email = self.request.POST.get('email')
+        profile = form.save(commit=False)
+        profile.save()
+        skills = form.cleaned_data.get('skill')
+        if skills:
+            print("===skilll===",skills)
+            pass
+            s=profile.skill.all()
+            if s:
+                profile.skill.remove(*s)
+            profile.skill.add(
+                *skills
+            )
+            # profile.skill.set(*skills, clear=True)
+
+        user = profile.user
+        user.email = email
+        user.save()
+        messages.success(self.request, 'Profile details updated successfully')
+        return redirect('settings', pk=profile.user.pk)
+
+    def form_invalid(self, form):
+        profile = self.get_object()
+        user = profile.user
+        return redirect('settings', pk=profile.user.pk)
 
 class JobPostView(FormView):
     form_class = JobCreateForm
